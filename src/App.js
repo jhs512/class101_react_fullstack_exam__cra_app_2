7cd7e2cbb12e38612e097ceb6039f585b4d94b68
jhs512 <jangka512@gmail.com>
2022-05-05 17:05:23 +0900

    테일윈드 도입

--- src/App.js
+++ src/App.js
@@ -5,9 +5,13 @@ import WritePage from "./pages/WritePage";
 function App() {
   return (
     <>
-      <header>
-        <NavLink to="/list">리스트</NavLink>
-        <NavLink to="/write">작성</NavLink>
+      <header className="flex">
+        <NavLink className="font-bold p-3 hover:text-red-500" to="/list">
+          리스트
+        </NavLink>
+        <NavLink className="font-bold p-3 hover:text-red-500" to="/write">
+          작성
+        </NavLink>
       </header>
       <Routes>
         <Route path="/list" element={<ListPage />} />
